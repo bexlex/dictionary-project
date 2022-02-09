45ae2c269d68fc5e417ecc89553f7f4f53a41add
bexlex <86270073+bexlex@users.noreply.github.com>
2022-02-08 22:05:10 -0500

    added synonymns

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Synonyms from "./Synonyms"
 
 export default function Meanings(props) {
 console.log(props.meanings)
     return (
         <div className="Meanings">
             <h3>{props.meanings.partOfSpeech}</h3>
-            <p>{props.meanings.definitions[0].definition}</p>
-            <p>{props.meanings.definitions[0].example}</p>
+            {props.meanings.definitions.map(function (definition, index)
+            {
+               return (
+                   <div key={index}>
+                       <p>{definition.definition}
+                       <br />
+                       <strong>Example: </strong>{definition.example}</p>
+                    <Synonyms synonyms={definition.synonyms} />
+                    </div>
+               ); 
+            })}
         </div>
     );
 }
