75ee2903165e15002900b660672e665b4c54688d
bexlex <86270073+bexlex@users.noreply.github.com>
2022-02-10 22:24:51 -0500

    integrate phonetics

--- src/Results.js
+++ src/Results.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Meanings from "./Meanings";
+import Phonetics from "./Phonetics";
 
 export default function Results(props){
     console.log(props.results);
     if (props.results) {
         return (
             <div className="Results">
-                            <h2>{props.results.word}</h2>
-    
+                <h2>{props.results.word}</h2>
+                {props.results.phonetics.map(function(phonetic,index) {
+                    return (
+                        <div key ={index}>
+                            <Phonetics phonetics={phonetic} />
+                        </div>
+                    );
+                })}    
+                
                 {props.results.meanings.map(function(meanings, index){
                     return (
                         <div key={index}>
